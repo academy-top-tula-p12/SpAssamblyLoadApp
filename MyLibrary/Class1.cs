28855aef3f818f6d0545888764aa3f169accf4f9
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-06-11 21:12:05 +0300

    Добавьте файлы проекта.

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace MyLibrary
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee() 
+        {
+            Name = "Anonim";
+            Age = 0;
+        }
+    }
+
+    public class Account
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; protected set; }
+
+        public Account()
+        {
+            Name = "Simple";
+            Balance = 0m;
+        }
+    }
+
+}
---
+++ SpAssamblyLoadApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace SpAssamblyLoadApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("MyLibrary.dll");
+
+            Console.WriteLine(assembly.FullName);
+
+            Type[] types = assembly.GetTypes();
+            foreach (Type t in types)
+                Console.WriteLine($"\t{t.Name}");
+        }
+    }
+}
